486ee6a4b1c0a8acc26bf438f374da1d9dcd1580
Anita <anita@11.com>
2015-09-08 15:29:36 +0800

    MyIOSFlappyBirdNew
    TomBin647/MyIOSFlappyBirdNew

---
+++ MyIOSFlappyBird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyIOSFlappyBird
+//
+//  Created by 高彬 on 15/9/8.
+//  Copyright (c) 2015年 erweimashengchengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyIOSFlappyBird/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MyIOSFlappyBird
+//
+
+//  Copyright (c) 2015年 erweimashengchengqi. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
